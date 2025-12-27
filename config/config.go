3f2a62a5b50e084dd426a094801736d3d99b8179
Hogant <Hogant8998@gmail.com>
2025-12-28 02:19:48 +0500

    configuration loader

---
+++ config/config.go
@@ -0,0 +1,50 @@
+package config
+
+import (
+	"flag"
+	"os"
+	"time"
+
+	"github.com/ilyakaznacheev/cleanenv"
+)
+
+type Config struct {
+	Env              string           `yaml:"env" env-default:"local"`
+	DbPath           string           `yaml:"database-path" env-required:"true"`
+	TelegramToken    string           `yaml:"telegram-token" env-required:"true"`
+	ContentProviders []ProviderConfig `yaml:"content-providers"`
+}
+
+type ProviderConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func LoadConfig() *Config {
+	configPath := fetchConfigPath()
+	if configPath == "" {
+		panic("config path is empty")
+	}
+
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		panic("config file does not exist: " + configPath)
+	}
+
+	var cfg Config
+
+	if err := cleanenv.ReadConfig(configPath, &cfg); err != nil {
+		panic("config path is empty: " + err.Error())
+	}
+
+	return &cfg
+}
+
+func fetchConfigPath() string {
+	var res string
+
+	flag.StringVar(&res, "config", "config.yaml", "path to config file")
+	flag.Parse()
+
+	return res
+}
